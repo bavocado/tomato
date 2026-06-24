90edfe93e7196f0c5d94f3de04d2323452564e74
ThomasZhang <alphejangs@gmail.com>
2026-06-24 15:44:21 +0800

    feat: initialize Go module with entry point

---
+++ cmd/root.go
@@ -0,0 +1,7 @@
+package cmd
+
+import "fmt"
+
+func Execute(version string) {
+	fmt.Printf("tomato version %s (not yet implemented)\n", version)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bavocado/tomato
+
+go 1.26.4
